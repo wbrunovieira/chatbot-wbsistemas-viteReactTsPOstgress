af806efbf176f278fe7433eea4c077389740e646
bruno-minimac <wbrunovieira77@gmail.com>
2024-09-12 16:58:13 -0300

    added tailwind

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,15 @@ WORKDIR /usr/src/app
 
 COPY package*.json ./
 
-RUN rm -rf node_modules package-lock.json
 
 RUN npm install
 
 COPY . .
 
-RUN npm run build
 
 RUN npm install -g serve
 
 EXPOSE 5173
 
-CMD ["serve", "-s", "dist", "-l", "5173"]
+
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-
-import './App.css'
-
 function App() {
-
-
-  return (
-    <>
-      <div>
-        <h1>chat bot</h1>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex justify-center align-center bg-primary">
+                <h1 className="text-red-50">chat bot 3</h1>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
